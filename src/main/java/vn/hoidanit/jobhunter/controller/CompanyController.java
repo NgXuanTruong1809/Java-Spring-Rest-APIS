5269024ec5470fde70aff613f50bd80615eac710
NgXuanTruong1809 <ngxuantruong1809@gmail.com>
2025-02-25 18:13:48 +0700

    paging

--- src/main/java/vn/hoidanit/jobhunter/controller/CompanyController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/CompanyController.java
@@ -4,13 +4,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import vn.hoidanit.jobhunter.domain.Company;
+import vn.hoidanit.jobhunter.domain.dto.ResultPaginationDTO;
 import vn.hoidanit.jobhunter.service.CompanyService;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class CompanyController {
@@ -23,6 +34,30 @@ public class CompanyController {
     @PostMapping("/companies")
     public ResponseEntity<Company> postCreateCompany(@Valid @RequestBody Company reqCompany) {
         return ResponseEntity.status(HttpStatus.CREATED).body(this.companyService.handleCreateCompany(reqCompany));
+    }
+
+    @GetMapping("/companies")
+    public ResponseEntity<ResultPaginationDTO> getCompany(
+            @RequestParam("current") Optional<String> currentOptional,
+            @RequestParam("pageSize") Optional<String> pageSizeOptional) {
+        int current = currentOptional.isPresent() == true ? Integer.parseInt(currentOptional.get())
+                : Integer.parseInt(null);
+        int pageSize = pageSizeOptional.isPresent() == true ? Integer.parseInt(pageSizeOptional.get())
+                : Integer.parseInt(null);
+
+        Pageable pageable = PageRequest.of(current - 1, pageSize);
+        return ResponseEntity.status(HttpStatus.OK).body(this.companyService.fetchAllCompanies(pageable));
+    }
+
+    @PutMapping("/companies")
+    public ResponseEntity<Company> putUpdateCompany(@Valid @RequestBody Company reqCompany) {
+        return ResponseEntity.ok(this.companyService.handleUpdateCompany(reqCompany));
+    }
+
+    @DeleteMapping("/companies/{id}")
+    public ResponseEntity<Void> deleteCompany(@PathVariable("id") long id) {
+        this.companyService.handleDeleteCompany(id);
+        return ResponseEntity.ok(null);
     }
 
 }
--- src/main/java/vn/hoidanit/jobhunter/service/CompanyService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/CompanyService.java
@@ -1,8 +1,15 @@
 package vn.hoidanit.jobhunter.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.jobhunter.domain.Company;
+import vn.hoidanit.jobhunter.domain.dto.Meta;
+import vn.hoidanit.jobhunter.domain.dto.ResultPaginationDTO;
 import vn.hoidanit.jobhunter.repository.CompanyRepository;
 
 @Service
@@ -15,5 +22,46 @@ public class CompanyService {
 
     public Company handleCreateCompany(Company company) {
         return this.companyRepository.save(company);
+    }
+
+    public ResultPaginationDTO fetchAllCompanies(Pageable pageable) {
+        Page<Company> pageCompany = this.companyRepository.findAll(pageable);
+
+        ResultPaginationDTO rsDTO = new ResultPaginationDTO();
+        Meta meta = new Meta();
+
+        meta.setCurrent(pageCompany.getNumber() + 1);
+        meta.setPageSize(pageCompany.getSize());
+
+        meta.setPages(pageCompany.getTotalPages());
+        meta.setTotal(pageCompany.getTotalElements());
+
+        rsDTO.setMeta(meta);
+        rsDTO.setResult(pageCompany.getContent());
+        return rsDTO;
+    }
+
+    public Company fetchById(long id) {
+        Optional<Company> company = this.companyRepository.findById(id);
+        if (company.isPresent()) {
+            return company.get();
+        }
+        return null;
+    }
+
+    public Company handleUpdateCompany(Company reqCompany) {
+        Company company = this.fetchById(reqCompany.getId());
+        if (company != null) {
+            company.setName(reqCompany.getName());
+            company.setDescription(reqCompany.getDescription());
+            company.setAddress(reqCompany.getAddress());
+            company.setLogo(reqCompany.getLogo());
+            company = this.handleCreateCompany(company);
+        }
+        return company;
+    }
+
+    public void handleDeleteCompany(Long id) {
+        this.companyRepository.deleteById(id);
     }
 }
