7f9b80c8fc41c989353f26fdfc59049b2054e98c
NgXuanTruong1809 <ngxuantruong1809@gmail.com>
2025-04-08 17:40:50 +0700

    FILE UPLOAD

--- src/main/java/vn/hoidanit/jobhunter/controller/FileController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/FileController.java
@@ -1,15 +1,47 @@
 package vn.hoidanit.jobhunter.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import vn.hoidanit.jobhunter.domain.dto.UploadFileDTO.ResUploadFileDTO;
+import vn.hoidanit.jobhunter.service.FileService;
+import vn.hoidanit.jobhunter.util.annotation.ApiMessage;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/v1")
 public class FileController {
+    private final FileService fileService;
+
+    @Value("${hoidanit.upload-file.base-uri}")
+    private String baseURI;
+
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
     @PostMapping("/files")
-    public String upload() {
-        return "upload";
+    @ApiMessage("Upload file")
+    public ResponseEntity<ResUploadFileDTO> upload(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("folder") String folder) throws URISyntaxException, IOException {
+
+        // Create a directory if not exist
+        this.fileService.createUploadFolder(baseURI + folder);
+        // store file
+        String uploadFile = this.fileService.store(file, folder);
+
+        ResUploadFileDTO resUploadFileDTO = new ResUploadFileDTO(uploadFile, Instant.now());
+        return ResponseEntity.ok().body(resUploadFileDTO);
     }
 
 }
